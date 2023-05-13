e2080c36f13358c0801a1f702d29678e56f7d15f
chhinhua <chhinhua@gmail.com>
2023-05-13 19:41:04 +0700

    add @CrossOrigin for category controller

--- src/main/java/com/example/mengrooming/controller/CategoryController.java
+++ src/main/java/com/example/mengrooming/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.example.mengrooming.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import java.util.List;
  * @author Chhin_Hua - 12/05
  **/
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1/categories")
 public class CategoryController {
